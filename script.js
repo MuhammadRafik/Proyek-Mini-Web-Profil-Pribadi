16e91988c7383e4bdbbcb20421b2e05a8f9feed0
MuhammadRafik <aeranaan@gmail.com>
2025-05-06 10:44:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function toggleBiodata() {
+    const sections = document.querySelectorAll("main section");
+    sections.forEach(sec => {
+        sec.style.display = sec.style.display === "none" ? "block" : "none";
+    });
+}
